402bc767a441b3e8fabed836ecc850c1b5deca3d
aju001 <94348613+aju001@users.noreply.github.com>
2021-11-15 20:13:07 +0530

    Ajuser🤍

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Julie Mwol*:  ```" + msg + "```"
+    return "✅ *Ajuser*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Julie Mwol*:  ```" + msg + "```"
+    return "🛑 *Ajuser*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Julie Mwol*:  ```" + msg + "```"
+    return "⏺️ *Ajuser*:  ```" + msg + "```"
 }
 
 
