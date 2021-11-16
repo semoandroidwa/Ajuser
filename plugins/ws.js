99414244865568c4afd4bdd3cb86999bf3504a49
Aju <94348613+aju001@users.noreply.github.com>
2021-11-16 17:29:08 +0530

    Ajuser🤍

--- plugins/ws.js
+++ plugins/ws.js
@@ -102,7 +102,7 @@ else if (Config.WORKTYPE == 'public') {
         
 // EXPORTING
         reply = await message.client.sendMessage(message.jid,'*꧁•⊹٭𝚄𝙿𝙻𝙾𝙰𝙳𝙸𝙽𝙶 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝚂𝚃𝙰𝚃𝚄𝚂٭⊹•꧂*',MessageType.text);
-        await message.client.sendMessage(message.jid, Buffer(respovideo.data), MessageType.video, {mimetype: Mimetype.mp4, caption: '◄━━━━━━━⦁⦁ *ᴍᴀᴅᴇ ʙʏ ɴᴀɴᴅʜᴜᴛᴛʏ* ⦁⦁━━━━━━━━►',})
+        await message.client.sendMessage(message.jid, Buffer(respovideo.data), MessageType.video, {mimetype: Mimetype.mp4, caption: '◄━━━━━━━⦁⦁ *ᴍᴀᴅᴇ ʙʏ ᴀᴊᴜsᴇʀ* ⦁⦁━━━━━━━━►',})
 
     }));
 
